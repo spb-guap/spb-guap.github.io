f752173ef8351dee9987a82344c7704fd57dd0a0
Traarbetare <traarbetare@gmail.com>
2025-09-22 20:34:52 +0300

    Today string always point to today

--- js/page/enumerations/renderRasp.js
+++ js/page/enumerations/renderRasp.js
@@ -13,4 +13,19 @@ export function renderRasp()
         `<p>Переход в основной раздел: <p/><a class="rasp-logo-a" href="/">${window.location.origin}</a>`
     );
     useModalWithImageOn('.btn-new-search', '/img/animated/u-piter.webp');
+
+    document.querySelector('.rasp-fourth-section__today').innerHTML=getTodayString();
+}
+
+function getTodayString() {
+    const days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+    const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    
+    const today = new Date();
+    const dayOfWeek = days[today.getDay()];
+    const day = today.getDate();
+    const month = months[today.getMonth()];
+    const year = today.getFullYear();
+    
+    return `Сегодня <nobr>${dayOfWeek}, ${day} ${month} ${year} г.</nobr><br>`;
 }
