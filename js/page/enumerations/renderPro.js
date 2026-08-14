cc5b64f7370805273939e8bbeb3e1ab1af9f6f27
Your Name <you@example.com>
2026-08-14 19:19:23 +0300

    useModalWithContentOn()

--- js/page/enumerations/renderPro.js
+++ js/page/enumerations/renderPro.js
@@ -9,4 +9,6 @@ export function renderPro()
     useModalOnAhrefsExceptFor(['a-logo-img', 'gayshit-lnk', 'lnk-a-jobs', 'tg-chat', 'tg-channel'], false);
     
     useNavigationRemappingOnTelegramRedirect();
+
+    useModalWithContentOn('.lnk-a-jobs', '<p class="text-centered">Эй парень, в приемку не хочешь устроиться?</p><img class="modal-content__image" src="/img/content/application-comission.jpg"/>')
 }
