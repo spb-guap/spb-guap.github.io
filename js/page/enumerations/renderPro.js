6e10379b16ffecc8a7bcecfcc57f7a64eb15acb1
Your Name <you@example.com>
2026-08-14 19:17:48 +0300

    fix error on not found content

--- js/page/enumerations/renderPro.js
+++ js/page/enumerations/renderPro.js
@@ -8,16 +8,5 @@ export function renderPro()
 {
     useModalOnAhrefsExceptFor(['a-logo-img', 'gayshit-lnk', 'lnk-a-jobs', 'tg-chat', 'tg-channel'], false);
     
-    useModalWithContentOn('.btn-new-search',
-        '<p class="modal-window__captcha-title"> Выберите все фото с резисторами на 220 Ω </p>' +
-            wrapImageStringIntoContainer('<img class="modal-window__image-adjusted" src="/img/rofls/220-resistors.jpg"/>'),
-        ['Далее', 'Пропустить'],
-        () => showPopupWithImage('<img class="modal-window__image" src="/img/animated/u-piter.webp"/>',
-            () => document.querySelector('.modal-window__background')?.classList.add('modal-window__horizontal-content')),
-        () => {
-            createInteractiveGrid('.modal-window__image-adjusted', 4, 4);
-        }
-    );
-
     useNavigationRemappingOnTelegramRedirect();
 }
